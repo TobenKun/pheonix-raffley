dc67ce4aca503b64976c77af9a6b70d77e7958c5
Sanghyun Shin <zxcv1867@naver.com>
2025-07-21 14:39:13 +0900

    mvp: add Rules module for rule page

---
+++ lib/raffley/rules.ex
@@ -0,0 +1,19 @@
+defmodule Raffley.Rules do
+  def list_rules do
+    [
+      %{
+        id: 1,
+        text: "Participants must have a high tolerance for puns and dad jokes.
+        😑"
+      },
+      %{
+        id: 2,
+        text: "Winner must do a victory dance when claiming their prize. 💃"
+      },
+      %{
+        id: 3,
+        text: "Have fun! 🤣"
+      }
+    ]
+  end
+end
--- lib/raffley_web/controllers/rule_controller.ex
+++ lib/raffley_web/controllers/rule_controller.ex
@@ -1,13 +1,16 @@
 defmodule RaffleyWeb.RuleController do
   use RaffleyWeb, :controller
 
+  alias Raffley.Rules
+
   def index(conn, _params) do
     emojis = ~W(😁 😅 🥸 🎃 💩 😴) |> Enum.random() |> String.duplicate(5)
 
     # 컨트롤러는 룰이 어떻게 구성되어야 하는지는 몰라도 됨
+    # 컨트롤러와 데이터 소스를 분리하는 것
     rules = Rules.list_rules()
 
     # assings the key-value to the conn
-    render(conn, :index, emojis: emojis)
+    render(conn, :index, emojis: emojis, rules: rules)
   end
 end
