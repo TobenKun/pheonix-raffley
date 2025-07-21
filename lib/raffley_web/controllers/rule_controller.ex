903966e98d077d3337f9b0864c94d18667f01dc0
Sanghyun Shin <zxcv1867@naver.com>
2025-07-21 13:40:22 +0900

    mvp: add style on rule page

--- lib/raffley_web/controllers/rule_controller.ex
+++ lib/raffley_web/controllers/rule_controller.ex
@@ -2,6 +2,12 @@ defmodule RaffleyWeb.RuleController do
   use RaffleyWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    emojis = ~W(😁 😅 🥸 🎃 💩 😴) |> Enum.random() |> String.duplicate(5)
+
+    # 컨트롤러는 룰이 어떻게 구성되어야 하는지는 몰라도 됨
+    rules = Rules.list_rules()
+
+    # assings the key-value to the conn
+    render(conn, :index, emojis: emojis)
   end
 end
