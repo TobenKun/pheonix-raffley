39779c739039781546eda2443704b41978ed4637
Toben <zxcv1867@naver.com>
2025-07-23 22:30:13 +0900

    comments: add comment for render function in live_view module

--- lib/raffley_web/live/estimator_live.ex
+++ lib/raffley_web/live/estimator_live.ex
@@ -9,6 +9,8 @@ defmodule RaffleyWeb.EstimatorLive do
     {:ok, socket}
   end
 
+  # render 함수를 지우고 estimator_live.html.heex 파일을 만들어서 거기 heex 템플릿을 넣어도 됨
+  # 피닉스는 live_view 모듈에서 render 함수가 없으면 이름이 동일한 heex파일을 찾아서 render 함수로 만들어줌(컴파일 시점에)
   def render(assigns) do
     ~H"""
     <div class="estimator">
