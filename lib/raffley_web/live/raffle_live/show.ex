dd5eb77ca90baba7b271040ee86368e3709c72d6
Toben <zxcv1867@naver.com>
2025-08-16 16:17:06 +0900

    mvp: use async data fetching on featured raffles

--- lib/raffley_web/live/raffle_live/show.ex
+++ lib/raffley_web/live/raffle_live/show.ex
@@ -5,27 +5,24 @@ defmodule RaffleyWeb.RaffleLive.Show do
   import RaffleyWeb.CustomComponents
 
   def mount(_params, _session, socket) do
-    IO.inspect(self(), label: "MOUNT")
     {:ok, socket}
   end
 
   def handle_params(%{"id" => id}, _uri, socket) do
-    IO.inspect(self(), label: "HANDLE PARAMS")
-
     raffle = Raffles.get_raffle!(id)
 
     socket =
       socket
       |> assign(:raffle, raffle)
       |> assign(:page_title, raffle.prize)
-      |> assign(:featured_raffles, Raffles.featured_raffles(raffle))
+      |> assign_async(:featured_raffles, fn ->
+        {:ok, %{featured_raffles: Raffles.featured_raffles(raffle)}}
+      end)
 
     {:noreply, socket}
   end
 
   def render(assigns) do
-    IO.inspect(self(), label: "RENDER")
-
     ~H"""
     <div class="raffle-show">
       <div class="raffle">
@@ -55,14 +52,26 @@ defmodule RaffleyWeb.RaffleLive.Show do
     ~H"""
     <section>
       <h4>Featured Raffles</h4>
-      <ul class="raffles">
-        <li :for={raffle <- @raffles}>
-          <.link navigate={~p"/raffles/#{raffle}"}>
-            <img src={raffle.image_path} />
-            {raffle.prize}
-          </.link>
-        </li>
-      </ul>
+      <.async_result :let={result} assign={@raffles}>
+        <:loading>
+          <div class="loading">
+            <div class="spinner"></div>
+          </div>
+        </:loading>
+        <:failed :let={{:error, reason}}>
+          <div :if={@raffles.failed} class="failed">
+            Yikes: {reason}
+          </div>
+        </:failed>
+        <ul class="raffles">
+          <li :for={raffle <- result}>
+            <.link navigate={~p"/raffles/#{raffle}"}>
+              <img src={raffle.image_path} />
+              {raffle.prize}
+            </.link>
+          </li>
+        </ul>
+      </.async_result>
     </section>
     """
   end
