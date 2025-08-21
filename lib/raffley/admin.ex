6a44f43e1ac6e19ab03c837d98ac6a916d78e662
Sanghyun Shin <zxcv1867@naver.com>
2025-08-21 21:36:12 +0900

    mvp: add create raffle feat on admin page

--- lib/raffley/admin.ex
+++ lib/raffley/admin.ex
@@ -8,4 +8,18 @@ defmodule Raffley.Admin do
     |> order_by(desc: :inserted_at)
     |> Repo.all()
   end
+
+  # FIX: 이렇게도 작동은 하지만, 아주 위험한 방법이다.
+  # 유저의 입력을 받는 부분은 충분히 검증해야함
+  # ex) 이름 없는 -3달러 짜리 raffle도 등록이 됨
+  def create_raffle(attrs \\ %{}) do
+    %Raffle{
+      prize: attrs["prize"],
+      description: attrs["description"],
+      ticket_price: attrs["ticket_price"] |> String.to_integer(),
+      status: attrs["status"] |> String.to_existing_atom(),
+      image_path: attrs["image_path"]
+    }
+    |> Repo.insert!()
+  end
 end
