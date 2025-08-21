f2497bddce300b80c78da68ca33a09c02e9e04cd
Sanghyun Shin <zxcv1867@naver.com>
2025-08-22 07:59:04 +0900

    mvp: modify Raffle schema changeset func

--- lib/raffley/raffles/raffle.ex
+++ lib/raffley/raffles/raffle.ex
@@ -17,5 +17,10 @@ defmodule Raffley.Raffles.Raffle do
     raffle
     |> cast(attrs, [:prize, :description, :ticket_price, :status, :image_path])
     |> validate_required([:prize, :description, :ticket_price, :status, :image_path])
+    |> validate_length(:description, min: 10)
+    |> validate_number(:ticket_price, greater_than_or_equal_to: 1)
   end
+
+  # 필요에 따라 다른 changeset 함수도 만들 수 있음
+  # ex) admin은 특정 데이터가 빠진 더미 데이터를 만들 수 있는 등
 end
